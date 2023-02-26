7d5e3876f1a6385581b5a921670deecc7d79d5e6
marimotona <ssmmo.06021224@gmail.com>
2023-02-26 22:49:53 +0900

    Add form

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const InputForm = () => {
+  return (
+    <div className="inputForm">
+        <form>
+            <input type="text">
+            <button>
+                <i class="fa-regular fa-circle-plus"></i>
+            </button>
+            </input>
+        </form>
+      
+    </div>
+  )
+}
+
+export default InputForm
