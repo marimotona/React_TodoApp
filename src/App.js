58856dcd94ce9d18672c89677b859a78db9723c7
marimotona <ssmmo.06021224@gmail.com>
2023-02-26 23:06:16 +0900

    Add todolist components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import InputForm from './components/InputForm';
 import Title from './components/Title';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <InputForm />
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div className='todoList'>
+        <div className='todos'>
+            <div className='todo'>
+                Hello World!
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default TodoList
